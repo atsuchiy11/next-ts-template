0e28370102a5a18eea4462e16d4fded33c5bbf01
atsuchiy11 <atsuchiy11@gmail.com>
2022-06-04 20:29:03 +0900

    init

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+    root: true,
+    env: {
+        browser: true,
+        es2021: true,
+        node: true
+    },
+    settings: {
+        react: { version: "detect"}
+    },
+    parser: "@typescript-eslint/parser",
+    parserOptions: {
+        ecmaFeatures: {
+            "jsx": true
+        },
+        ecmaVersion: "latest",
+        sourceType: "module"
+    },
+    extends: [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:@typescript-eslint/recommended",
+        "plugin:prettier/recommended"
+    ],    
+    plugins: [
+        "react",,
+        "@typescript-eslint",
+    ],
+    rules: {
+        // prettierのルール
+        "semi": [2, "always"],
+        "prettier/prettier": ["error", {
+            "semi": true,
+            "singleQuote": true,
+            "trailingComma": "all",
+            "printWidth": 100,
+        }],
+        // reactのカスタムルール
+        "react/prop-types": "off", // TSなので不要
+        "react/react-in-jsx-scope": "off" // v17からReactはグローバル
+    }
+};
